e9fba0e36aa9c0fc7da5ad7b15531c1e848e1b43
radfre <schatz.alex@gmail.com>
2023-03-03 01:56:38 -0600

    added shaes on close out

---
+++ js/site.js
@@ -0,0 +1,18 @@
+const overlay = document.getElementById("overlay");
+const circle = document.getElementById("circle");
+const square = document.getElementById("square");
+const triangle = document.getElementById("triangle")
+const circle2 = document.getElementById("circle2");
+const square2 = document.getElementById("square2");
+const triangle2 = document.getElementById("triangle2")
+
+
+function showOverlay() {
+    overlay.style.display = "block";
+    circle.style.display = "block";
+    square.style.display = "block";
+    triangle.style.display = "block";
+    circle2.style.display = "block";
+    square2.style.display = "block";
+    triangle2.style.display = "block";
+}
