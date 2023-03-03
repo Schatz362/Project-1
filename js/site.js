7dcb2eeb4ff748bfcbdbc41cd98e9302deb5ea4f
radfre <schatz.alex@gmail.com>
2023-03-03 02:41:26 -0600

    added logic to page load to force require completion

--- js/site.js
+++ js/site.js
@@ -7,14 +7,30 @@ const square2 = document.getElementById("square2");
 const triangle2 = document.getElementById("triangle2")
 const message = document.getElementById("message")
 
-
 function showOverlay() {
-    overlay.style.display = "block";
-    circle.style.display = "block";
-    square.style.display = "block";
-    triangle.style.display = "block";
-    circle2.style.display = "block";
-    square2.style.display = "block";
-    triangle2.style.display = "block";
-    message.style.display = "block";
+    const form = document.querySelector('#shape-form');
+    const elements = form.querySelectorAll('input');
+    let formCompleted = true;
+
+    elements.forEach(element => {
+        if (element.hasAttribute('required') && !element.value) {
+            formCompleted = false;
+        } else if (element.hasAttribute('pattern')) {
+            const pattern = new RegExp(element.getAttribute('pattern'));
+            if (!pattern.test(element.value)) {
+                allCompleted = false;
+            }
+        }
+    });
+
+    if(formCompleted) {
+        overlay.style.display = "block";
+        circle.style.display = "block";
+        square.style.display = "block";
+        triangle.style.display = "block";
+        circle2.style.display = "block";
+        square2.style.display = "block";
+        triangle2.style.display = "block";
+        message.style.display = "block";
+    }
 }
